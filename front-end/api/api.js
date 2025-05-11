90275eb39da058df50740874a8d77983d15f69dd
lucase10 <lucase10.1010@gmail.com>
2025-05-11 18:23:37 -0300

    Ajustes no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,12 +1,10 @@
 // fetch ou Axios
-import "dotenv/config";
+// import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
-
-// const URL = "http://localhost:3001";
-// const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+// const { NODE_ENV } = process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://deploy-jornada-full-stack-wzx1.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
